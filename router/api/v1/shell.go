033fd99102511ca1a2ea89822fb89093682b38a5
井上大誠 <t.base.8901@icloud.com>
2021-07-11 20:20:05 +0900

    dev用コメント追記

--- router/api/v1/shell.go
+++ router/api/v1/shell.go
@@ -7,6 +7,8 @@ import(
 
 func PostCmd(c *gin.Context) {
 	cmd := c.PostForm("cmd")
+	// judge.Judge(cmd)
+	// judgeで判定。正解だったら勝ち、その他のユーザに負けを通知する。
 	out, err := exec.Command("bash", "-c", cmd).Output()
 	if err != nil {
 		c.JSON(500, gin.H{
