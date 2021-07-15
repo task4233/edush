6b1e0b736cba0af366f80e1fb28bbd06a3ce7749
井上大誠 <t.base.8901@icloud.com>
2021-07-15 19:33:00 +0900

    add docker exec command

--- practice/container.go
+++ practice/container.go
@@ -28,6 +28,7 @@ func DockerPs() {
 	}
 }
 
+// dockerのバージョン確認例
 func DockerVersion() {
 	cli, err := client.NewClientWithOpts(client.FromEnv)
 	if err != nil {
@@ -37,7 +38,8 @@ func DockerVersion() {
 	fmt.Printf("%s\n", v)
 }
 
-func DockerRun() {
+//コンテナを起動する例
+func DockerRunSample() {
 	cli, err := client.NewClientWithOpts(client.FromEnv)
 	if err != nil {
 		panic(err)
@@ -64,11 +66,66 @@ func DockerRun() {
 	if err := cli.ContainerStart(context.Background(), body.ID, types.ContainerStartOptions{}); err != nil {
 		panic(err)
 	}
-	//起動してます。
 	time.Sleep(20 * time.Second)
-	//終了処理に入ります。
 	timeout := 30 * time.Second
 	if err := cli.ContainerStop(context.Background(), "test_container", &timeout); err != nil {
 		panic(err)
 	}
+}
+
+
+//コンテナを起動する。
+func DockerRun(cli *client.Client) (string, error) {
+	bg := context.Background()
+	cc := &container.Config{
+		Image:        "nginx",
+		ExposedPorts: nat.PortSet{nat.Port("80"): struct{}{}},
+	}
+	hc := &container.HostConfig{
+		PortBindings: nat.PortMap{
+			nat.Port("80"): []nat.PortBinding{nat.PortBinding{HostPort: "8080"}},
+		},
+		AutoRemove: true,
+	}
+	body, err := cli.ContainerCreate(bg, cc, hc, nil, nil, "test_container")
+	if err != nil {
+		return "", err
+	}
+
+	if err := cli.ContainerStart(bg, body.ID, types.ContainerStartOptions{}); err != nil {
+		return "", err
+	}
+	return body.ID, nil
+}
+/**課題
+ユーザに紐つける必要があるもの
+- コンテナのID
+- カレントディレクトリ
+**/
+
+//マンドを引数にとってコンテナ内でそのコマンドを実行させる方法例
+/**
+id: コンテナID DockerRun()の戻り値とか
+**/
+func DockerExec(id string, cli *client.Client) error {
+	
+	ec := &types.ExecConfig{
+		User: "hoge",
+		Privileged: false,
+		Tty: true,
+		WorkingDir: "/",
+		Cmd: []string{"ls", "-a"},
+	}
+	idResponse, err := cli.ContainerExecCreate(context.Background(), id, *ec)
+	if err != nil {
+		return err
+	}
+	execStartCheck := types.ExecStartCheck{
+		Detach: true,
+		Tty:    false,
+	}
+	if err = cli.ContainerExecStart(context.Background(), idResponse.ID, execStartCheck); err != nil {
+		return err
+	}
+	return nil
 }
